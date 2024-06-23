7cbc98e1b129066cc8fcca55ba8de337c7a897b9
kyoamat <ktatsuki198@gmail.com>
2024-06-23 23:58:17 +0900

    chore: Update Header component and remove middleware.ts

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { defineMiddleware } from "astro:middleware";
-
-export const onRequest = defineMiddleware((context, next) => {
-  if (context.url.pathname === "/") {
-    return Response.redirect(new URL("/blog", context.url), 302);
-  }
-
-  return next();
-});
