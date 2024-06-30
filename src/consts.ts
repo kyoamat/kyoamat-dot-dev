0f22ac0fa140bbc7d871e5fab5499cd84497c69a
kyoamat <ktatsuki198@gmail.com>
2024-07-01 01:07:41 +0900

    chore: Add support for tags in blog collection schema

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,10 @@
 export const SITE_TITLE = "KYOAMAT.DEV";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const TAGS: Readonly<string[]> = [
+  "プログラミング",
+  "ソフトウェア開発",
+  "日々のつぶやき",
+  "メモ",
+  "日常生活",
+  "雑記",
+];
