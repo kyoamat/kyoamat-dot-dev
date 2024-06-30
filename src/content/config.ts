59de55be5d6196cc7c629cafd99f015b13634e9c
kyoamat <ktatsuki198@gmail.com>
2024-07-01 01:12:30 +0900

    chore: Make tags optional in blog collection schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,7 +7,7 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
-    tags: z.array(z.enum([TAGS[0], ...TAGS.slice(1)])),
+    tags: z.array(z.enum([TAGS[0], ...TAGS.slice(1)])).optional(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
   }),
